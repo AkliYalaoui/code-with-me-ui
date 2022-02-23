1cfe6ff1274d61dd50b92e5e588c0b806e51711f
AkliYalaoui <akliyalaoui@gmail.com>
2022-02-23 02:25:59 +0100

    project download feature

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -15,7 +15,7 @@ const Project = () => {
   const [js, setJs] = useState("");
   const [srcDoc, setsrcDoc] = useState("");
   const { id } = useParams();
-  const [contributers,setContributers] = useState([]);
+  const [contributers, setContributers] = useState([]);
   const [socket, setSocket] = useState();
 
   useEffect(() => {
@@ -46,11 +46,15 @@ const Project = () => {
         <script>${js}</script>
         </html>
       `);
-      socket?.emit("send-code", id, {html : html ? html :"",css: css ? css :"",js : js ? js : ""});
+      socket?.emit("send-code", id, {
+        html: html ? html : "",
+        css: css ? css : "",
+        js: js ? js : "",
+      });
     }, 300);
 
     return () => clearTimeout(timeout);
-  }, [socket,html, css, js]);
+  }, [socket, html, css, js]);
 
   useEffect(() => {
     const getProject = async () => {
@@ -68,7 +72,7 @@ const Project = () => {
           setHtml(data.project.html);
           setCss(data.project.css);
           setJs(data.project.js);
-          setContributers(prev => [...prev,...data.project.contributers]);
+          setContributers((prev) => [...prev, ...data.project.contributers]);
         }
       } catch (err) {
         console.error(err);
@@ -78,55 +82,80 @@ const Project = () => {
     getProject();
   }, [id]);
 
-  const saveProject = async () =>{
-      if(!html.trim() && !css.trim() && !js.trim())
-        return;
-
+  const download = async () => {
     try {
-        const res = await fetch(`${BASE_URL}/api/project/content/${id}`, {
-          method: "PUT",
-          body: JSON.stringify({html,css,js}),
-          headers: {
-            authorization: `Bearer ${user.token}`,
-            "Content-Type" : "application/json"
-          },
-        });
-        const data = await res.json();
-        if (data.status != 200) {
-          setError(data.message);
-        } else {
-            console.log(data.message);
-        }
-      } catch (err) {
-        console.error(err);
-        setError("Oups something Went Wrong :(");
+      const res = await fetch(`${BASE_URL}/api/project/download/${id}`, {
+        headers: {
+          authorization: `Bearer ${user.token}`,
+        },
+      });
+      const data = await res.text();
+      if (data.status && data.status != 200) {
+        setError(data.message);
+      } else {
+        console.log(data);
+        const url = window.URL.createObjectURL(new Blob([data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "project.html");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
       }
+    } catch (err) {
+      console.error(err);
+      setError("Oups something Went Wrong :(");
+    }
   };
-  const addContributer = async () =>{
-      const contributerId = prompt("Enter Your Contributer Id");
-      if(!contributerId)
-        return;
+
+  const saveProject = async () => {
+    try {
+      const res = await fetch(`${BASE_URL}/api/project/content/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ html : html ? html: "", css : css ? css:"", js: js ? js:"" }),
+        headers: {
+          authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (data.status != 200) {
+        setError(data.message);
+      } else {
+        console.log(data.message);
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Oups something Went Wrong :(");
+    }
+  };
+  const addContributer = async () => {
+    const contributerId = prompt("Enter Your Contributer Id");
+    if (!contributerId) return;
 
     try {
-        const res = await fetch(`${BASE_URL}/api/project/add/contributer/${id}`, {
-          method: "POST",
-          body: JSON.stringify({contributerId}),
-          headers: {
-            authorization: `Bearer ${user.token}`,
-            "Content-Type" : "application/json"
-          },
-        });
-        const data = await res.json();
-        if (data.status != 200) {
-          setError(data.message);
-        } else {
-            console.log(data.message);
-            setContributers(prevContributers => [...prevContributers,data.newContributer]);
-        }
-      } catch (err) {
-        console.error(err);
-        setError("Oups something Went Wrong :(");
+      const res = await fetch(`${BASE_URL}/api/project/add/contributer/${id}`, {
+        method: "POST",
+        body: JSON.stringify({ contributerId }),
+        headers: {
+          authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (data.status != 200) {
+        setError(data.message);
+      } else {
+        console.log(data.message);
+        setContributers((prevContributers) => [
+          ...prevContributers,
+          data.newContributer,
+        ]);
       }
+    } catch (err) {
+      console.error(err);
+      setError("Oups something Went Wrong :(");
+    }
   };
 
   return (
@@ -134,14 +163,18 @@ const Project = () => {
       {error ? (
         <Error e={error} />
       ) : (
-        <div> 
-            <section className="flex justify-center items-center mb-4">
-              {contributers.map(contributer => (
-                  <div key={contributer._id} className="m-2 text-center">
-                      <div className="rounded-full bg-gray-900 text-center text-white p-2 h-10 w-10">{contributer.username[0].toUpperCase()}</div>
-                      <span className="opacity-75">{contributer.username.slice(0,8)}</span>
-                    </div>
-              ))}
+        <div>
+          <section className="flex justify-center items-center mb-4">
+            {contributers.map((contributer) => (
+              <div key={contributer._id} className="m-2 text-center">
+                <div className="rounded-full bg-gray-900 text-center text-white p-2 h-10 w-10">
+                  {contributer.username[0].toUpperCase()}
+                </div>
+                <span className="opacity-75">
+                  {contributer.username.slice(0, 8)}
+                </span>
+              </div>
+            ))}
           </section>
 
           <header className="flex items-center justify-between">
@@ -150,8 +183,26 @@ const Project = () => {
               <p className="opacity-80">- {project.description}</p>
             </div>
             <div className="space-x-2">
-            {project.userId === user._id && <button onClick={addContributer} className="px-5 py-3 cursor-pointer bg-gray-900 text-white rounded hover:opacity-80">share</button>}
-            <button onClick={saveProject} className="px-5 py-3 cursor-pointer bg-green-600 text-white rounded hover:opacity-80">save</button>
+              {project.userId === user._id && (
+                <button
+                  onClick={addContributer}
+                  className="px-5 py-3 cursor-pointer bg-gray-900 text-white rounded hover:opacity-80"
+                >
+                  share
+                </button>
+              )}
+              <button
+                onClick={download}
+                className="px-5 py-3 cursor-pointer bg-green-600 text-white rounded hover:opacity-80"
+              >
+                download
+              </button>
+              <button
+                onClick={saveProject}
+                className="px-5 py-3 cursor-pointer bg-blue-600 text-white rounded hover:opacity-80"
+              >
+                save
+              </button>
             </div>
           </header>
           <section className="mt-4">
@@ -170,8 +221,7 @@ const Project = () => {
                 onChange={setJs}
               />
             </div>
-            <div
-              className="border border-gray-400">
+            <div className="border border-gray-400">
               <iframe
                 style={{ minHeight: "300px" }}
                 srcDoc={srcDoc}
